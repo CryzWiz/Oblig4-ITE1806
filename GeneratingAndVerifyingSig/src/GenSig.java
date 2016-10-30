834741e1e5c3dbbf5e9d35231b6d72271314b1a8
Allan Arnesen <allan.arnesen@gmail.com>
2016-10-30 20:16:33 +0100

    Prelim Final

--- GeneratingAndVerifyingSig/src/GenSig.java
+++ GeneratingAndVerifyingSig/src/GenSig.java
@@ -4,65 +4,52 @@ import java.security.cert.CertificateException;
  
 class GenSig {
 	public static String keystorage = "keystore";
+	
     public static void main(String[] args) {
     	final char[] password= args[1].toCharArray();
         /* Generate a DSA signature */
- 
         if (args.length != 2) {
             System.out.println("Usage: GenSig nameOfFileToSign password");
             }
         else try{
- 
-            /* Generate a key pair */
-        	
-            //KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
-        	KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "SunRsaSign");
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
- 
-            keyGen.initialize(1024, random);
- 
+            /* Get the private key from the vault 
+             * 
+             */
             PrivateKey pvt = getPrivate("TheVault", password);
+            /* Get the public key from the vault
+             * 
+             */
             PublicKey pub = getPublic("TheVault", password);
- 
- 
-            /* Create a Signature object and initialize it with the private key */
-            
-            //Signature dsa = Signature.getInstance("SHA1withDSA", "SUN"); 
-            Signature dsa = Signature.getInstance("SHA256WITHRSA", "SunRsaSign");
-            
-            dsa.initSign(pvt);
+  
+            /* Create a Signature object and initialize it with the private key 
+             */
+            Signature rsa = Signature.getInstance("SHA256WITHRSA", "SunRsaSign");         
+            rsa.initSign(pvt);
  
             /* Update and sign the data */
- 
             FileInputStream fis = new FileInputStream(args[0]);
             BufferedInputStream bufin = new BufferedInputStream(fis);
             byte[] buffer = new byte[1024];
             int len;
             while (bufin.available() != 0) {
                 len = bufin.read(buffer);
-                dsa.update(buffer, 0, len);
+                rsa.update(buffer, 0, len);
                 };
- 
             bufin.close();
  
             /* Now that all the data to be signed has been read in, 
-                    generate a signature for it */
- 
-            byte[] realSig = dsa.sign();
- 
-         
+             * generate a signature for it 
+             * */
+            byte[] realSig = rsa.sign();
             /* Save the signature in a file */
             FileOutputStream sigfos = new FileOutputStream("Allans-Signatur");
             sigfos.write(realSig);
- 
             sigfos.close();
- 
  
             /* Save the public key in a file */
             byte[] key = pub.getEncoded();
             FileOutputStream keyfos = new FileOutputStream("Allans-PubKey");
             keyfos.write(key);
- 
             keyfos.close();
  
         } catch (Exception e) {
@@ -70,15 +57,19 @@ class GenSig {
         }
  
     }
-
-	static PublicKey getPublic(String alias, char[] password) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+    /* Get the public key - shared method with VerSig.
+     * 
+     */
+	public static PublicKey getPublic(String alias, char[] password) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		KeyStore ks = KeyStore.getInstance("JKS");
 		FileInputStream ksfis = new FileInputStream(keystorage);
 		BufferedInputStream ksbufin = new BufferedInputStream(ksfis);
 		ks.load(ksbufin, password);
 		return ks.getCertificate(alias).getPublicKey();
 	}
-
+	/* Get the private Key
+	 * 
+	 */
 	private static PrivateKey getPrivate(String alias, char[] password) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException {
 		KeyStore ks = KeyStore.getInstance("JKS");
 		FileInputStream ksfis = new FileInputStream(keystorage);
--- GeneratingAndVerifyingSig/src/VerSig.java
+++ GeneratingAndVerifyingSig/src/VerSig.java
@@ -7,52 +7,46 @@ class VerSig {
 	public static String keystorage = "keystore";
     public static void main(String[] args) {
  
-        /* Verify a DSA signature */
+        /* Verify a RSA signature */
         if (args.length != 4) {
             System.out.println("Usage: VerSig alias password signaturefile datafile");
             }
         else try{
-        	String alias = args[0];
         	String pw = args[1];
         	char[] password = pw.toCharArray();
         	String signaturefile = args[2];
         	String datafile = args[3];
         	
-        	/* import encoded public key */
-        	
+        	/* import encoded public key - Using GenSigs method for public key
+        	 * Since this is for just me, I can have it just like this.
+        	 * If not you'll need a public method for this VerSig and supply 
+        	 * the password for you key storage
+        	 */
             PublicKey pubKey = GenSig.getPublic(args[0], password);
             
             /* input the signature bytes */
             FileInputStream sigfis = new FileInputStream(signaturefile);
             byte[] sigToVerify = new byte[sigfis.available()]; 
             sigfis.read(sigToVerify );
- 
             sigfis.close();
- 
+            
             /* create a Signature object and initialize it with the public key */
             //Signature sig = Signature.getInstance("SHA1WITHDSA", "SUN");
             Signature sig = Signature.getInstance("SHA256WITHRSA", "SunRsaSign");
             sig.initVerify(pubKey);
  
             /* Update and verify the data */
- 
             FileInputStream datafis = new FileInputStream(datafile);
             BufferedInputStream bufin = new BufferedInputStream(datafis);
- 
             byte[] buffer = new byte[1024];
             int len;
             while (bufin.available() != 0) {
                 len = bufin.read(buffer);
                 sig.update(buffer, 0, len);
                 };
- 
             bufin.close();
- 
- 
             boolean verifies = sig.verify(sigToVerify);
- 
             System.out.println("signature verifies: " + verifies);
- 
  
         } catch (Exception e) {
             System.err.println("Caught exception " + e.toString());
